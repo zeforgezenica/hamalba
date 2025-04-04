d0289e24c294d8e01cc8ce790faec72593ce900f
Harun225883 <kadic.harun.21@size.ba>
2025-04-04 14:43:35 +0200

    Prvobitna verzija modela
    Uradjena pomocu asp.net identitija , trenutno ima sad korisnik

--- Program.cs
+++ Program.cs
@@ -1,27 +1,45 @@
 using hamalba.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using hamalba.Models;
+using hamalba.DataBase;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Registruj servise
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<DatabaseService>();
 
+// Entity Framework + MySQL (Pomelo)
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(9, 0, 0)) // prilagodi ako tvoj MySQL nije verzija 9
+    ));
+
+// ASP.NET Identity setup
+builder.Services.AddIdentity<Korisnik, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//HTTP pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
 
+//  Autentifikacija i autorizacija
+app.UseAuthentication(); // MORA biti prije Authorization
 app.UseAuthorization();
 
+// Rute
 app.MapStaticAssets();
 
 app.MapControllerRoute(
@@ -29,5 +47,5 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
-
 app.Run();
+
