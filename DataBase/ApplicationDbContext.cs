5cef4794c0843494e55e61187f68089198d23650
Edin Omanovic <127051224+EdinOmanovic@users.noreply.github.com>
2025-04-04 15:01:24 +0200

    Dodan model oglas

---
+++ DataBase/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using hamalba.Models;
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using hamalba.Models;
+
+namespace hamalba.DataBase
+{
+    public class ApplicationDbContext : IdentityDbContext<Korisnik>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Oglas> Oglasi { get; set; }
+
+        
+    }
+
+}
---
+++ Models/Korisnik.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace hamalba.Models
+{
+    public class Korisnik : IdentityUser
+    {
+        public string Ime { get; set; }
+        public string Adresa { get; set; }
+        public DateTime DatumRegistracije { get; set; } = DateTime.UtcNow;
+        public bool Verifikovan { get; set; }
+    }
+}
